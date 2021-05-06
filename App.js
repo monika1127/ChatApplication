11f83aea6376048e999fe821f2a3012b5b2bae03
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-05-06 23:08:23 +0200

    ApolloClient install

--- App.js
+++ App.js
@@ -1,21 +1,32 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import Room from './Components/Rooms'
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+import Room from "./Components/Rooms";
+import { ApolloClient, InMemoryCache } from "@apollo/client";
+import { ApolloProvider } from "@apollo/client/react";
+
+
+// const client = ...
+const client = new ApolloClient({
+  uri: 'https://48p1r2roz4.sse.codesandbox.io',
+  cache: new InMemoryCache(),
+});
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <ApolloProvider client={client}>
+      <View style={styles.container}>
         <Room />
-    </View>
+      </View>
+    </ApolloProvider>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+    backgroundColor: "#fff",
+    alignItems: "center",
+    justifyContent: "center",
   },
 });
