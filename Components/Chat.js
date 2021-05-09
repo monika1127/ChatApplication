e68bbb8ee7b8194049fc888e865f5edc6d075dce
monika1127 <66638216+monika1127@users.noreply.github.com>
2021-05-09 03:04:29 +0200

    Chat styling

--- Components/Chat.js
+++ Components/Chat.js
@@ -1,6 +1,7 @@
 import React, { useState, useCallback, useEffect } from "react";
 import { View, StyleSheet } from "react-native";
-import { GiftedChat } from "react-native-gifted-chat";
+import {  GiftedChat, Send, InputToolbar, Composer } from "react-native-gifted-chat";
+import SendIcon from "../assets/send.svg";
 import { SEND_MESSAGE, ROOM_MESSAGES, MESSAGE_ADDED } from "../queries/queries";
 import { useMutation, useQuery, useSubscription } from "@apollo/client";
 import { formatMessage } from "./helpers/formatters";
@@ -30,23 +31,46 @@ export default function Chat({ route }) {
     await sendMessage({ variables: { body: text, roomId } });
   }, []);
 
-
-// subscription and state update when new message
+  // subscription and state update when new message
   const { data: subData } = useSubscription(MESSAGE_ADDED, {
     variables: { roomId },
   });
 
   useEffect(() => {
     if (subData) {
-      const newMessage = [formatMessage(subData.messageAdded)]
+      const newMessage = [formatMessage(subData.messageAdded)];
       setMessages((prev) => GiftedChat.append(prev, newMessage));
     }
   }, [subData, setMessages]);
 
+  // functions for chat styling
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View style={styles.sendingContainer}>
+
+                  <SendIcon />
+        </View>
+      </Send>
+    );
+  };
+
+  const renderInputToolbar = (props)=> {
+      return <InputToolbar {...props} containerStyle={styles.msgContainer} />
+  }
+
   if (!user) return null;
   return (
     <View style={styles.container}>
-      <GiftedChat messages={messages} onSend={onSend} user={{ _id: user.id }} />
+      <GiftedChat
+        messages={messages}
+        onSend={onSend}
+        alwaysShowSend={true}
+        user={{ _id: user.id }}
+        renderSend={renderSend}
+        renderInputToolbar={renderInputToolbar}
+        textInputStyle={styles.msgInput}
+      />
     </View>
   );
 }
@@ -58,4 +82,27 @@ const styles = StyleSheet.create({
     justifyContent: "flex-start",
     width: "100%",
   },
+  sendingContainer: {
+    justifyContent: 'center',
+    alignItems: "flex-end",
+
+  },
+  msgContainer: {
+    backgroundColor: "#b6defd",
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    paddingVertical:4,
+    paddingHorizontal: 8
+  },
+  msgInput: {
+    backgroundColor: "white",
+    borderTopLeftRadius: 16,
+    borderTopRightRadius: 16,
+    borderBottomLeftRadius: 16,
+    paddingHorizontal: 16,
+    paddingVertical: 4,
+    marginVertical: 8,
+    maxHeight: 38
+
+  }
 });
